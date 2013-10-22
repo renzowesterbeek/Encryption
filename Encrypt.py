db42bb5881ade781f3fdec3a5d8a1b42259e56bb
Renzo <renzowesterbeek@gmail.com>
2013-10-22 12:29:31 +0200

    Reworked the program

--- Encrypt.py
+++ Encrypt.py
@@ -1,30 +1,18 @@
 alfabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-def encrypt(userString):
-	string = ""
-	toAdd = 2
-	for c in userString:
 	
-		indexNumber = alfabet.index(c)
-		if c == " ":
-			string = string + ":"
-		elif indexNumber >= 25 :
+def encrypt(originalString):
+	toAdd = 1
+	endString = ""
+	for c in originalString:
+		indexNum = alfabet.index(c)
+		encNum = indexNum + toAdd
+		
+		if encNum > 25:
+			encNum = encNum - 26
 			
-			print indexNumber
-			# Encrypt
-			newNumber = indexNumber - 25
-
-			# Output
-			c = alfabet[newNumber]
-			string = string + c
-		else:
-			# Encrypt
-			newNumber = indexNumber + toAdd
-
-			# Output
-			c = alfabet[newNumber]
-			string = string + c
-	print string
+		endString += alfabet[encNum]
+		
+	print "Your encrypted word is: " + endString
 			
 
 userString = raw_input("String to encrypt ")
