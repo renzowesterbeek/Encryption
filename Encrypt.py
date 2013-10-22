47ef23faa640bcc813825e2377bafa703aa59d0a
Renzo <renzowesterbeek@gmail.com>
2013-10-22 13:50:11 +0200

    Added random and comments
    - Added generation of random numbers and saving it to output file
    - Added comments for better understanding

--- Encrypt.py
+++ Encrypt.py
@@ -1,13 +1,24 @@
+# Define functions, imports and lists
+import random
+
+# List with all letters for alfabet
 alfabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-	
+
+# Function for saving words
+def saveOutput(originalString, toAdd, endString):
+	in_file = open("output.txt", "a")
+	text = in_file.write(originalString + "    " + str(toAdd) + "    " + endString + '\n')
+
+# Function for encryption
 def encrypt(originalString):
-	toAdd = 1
+	toAdd = random.randint(1, 20)
 	endString = ""
 	for c in originalString:
 	
 		if c == " ":
 			endString += ":"
 		else:
+			# Finds indexnumber and encrypts it
 			indexNum = alfabet.index(c)
 			encNum = indexNum + toAdd
 		
@@ -15,11 +26,15 @@ def encrypt(originalString):
 				encNum = encNum - 26
 			
 			endString += alfabet[encNum]
-		
+	
+	# Saves and prints word	
 	print "Your encrypted word is: " + endString
-			
+	saveOutput(originalString, toAdd, endString)
+	
 
-userString = raw_input("String to encrypt ")
+# Asks for input
+userString = raw_input("String to encrypt: ")
 userString = userString.lower()
 
+# Calls encrypt function
 encrypt(userString)
